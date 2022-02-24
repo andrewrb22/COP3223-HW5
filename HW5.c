8f6fc2abcd0350b455268eb65eb178b2e478ad3c
Andrew Rodriguez <andrewrb22@gmail.com>
2022-02-24 08:16:11 -0500

    HW5 TESTING

--- HW5.c
+++ HW5.c
@@ -5,15 +5,32 @@ int main()
 {
 
     float *p;
+    float *q;
+    char *c;
+
 
     p = malloc(sizeof(float));
+    c = malloc(sizeof(char));
+    q = malloc (sizeof(float));
 
-    printf("Enter your number: ");
-    scanf("%f", &*p);
+    printf("Enter your two Integers:\n ");
+    scanf("%f %f", &*p, &*q);
 
-    printf("You have enter: %.2f\n", *p);
 
-    printf("Double of the number you enter is: %.2f\n", 2 * *p);
+
+
+
+    printf("Now, enter a or A to show the sum, p or P to show the product or d or D to show the division of the first by the second: \n");
+    scanf("%c", &*c);
+    
+
+    // if (*c == 'a' || *c == 'A'){
+    //     printf("%f + %f  = %.2f", *p, *q, *p + *q);
+    // }
+
+
+
 
     return 0;
 }
+// printf("You have enter: %.2f\n", *p);
