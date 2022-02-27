18dfabd6fad71b738803725944acb506b1949f96
Andrew Rodriguez <andrewrb22@gmail.com>
2022-02-27 16:29:05 -0500

    Created division case with 0 validation;

--- HW5.c
+++ HW5.c
@@ -8,49 +8,59 @@ int main()
     float *q;
     char options;
 
-
     p = malloc(sizeof(*p));
-    q = malloc (sizeof(*q));
+    q = malloc(sizeof(*q));
 
     printf("Enter your two Integers:\n ");
     scanf("%f %f", &*p, &*q);
 
     printf("Now, enter the following options:\na or A to show the sum\np or P to show the product\nd or D to show the division of the first by the second: ");
-    scanf(" %c",&options);
+    scanf(" %c", &options);
 
-switch (options)
-{
-case 'A':
-    printf("\n %.2f + %.2f is = %.2f\n", *p , *q , *p + *q);
-    break;
-
-case 'a':
-    printf("\n %.2f + %.2f is = %.2f\n", *p , *q , *p + *q);
-    break;
+    switch (options)
+    {
+    case 'A':
+        printf("\n %.2f + %.2f is = %.2f\n", *p, *q, *p + *q);
+        break;
+    case 'a':
+        printf("\n %.2f + %.2f is = %.2f\n", *p, *q, *p + *q);
+        break;
     case 'P':
-    printf("\n %.2f * %.2f = %.2f\n", *p, *q, *p * *q);
-    break;
+        printf("\n %.2f * %.2f = %.2f\n", *p, *q, *p * *q);
+        break;
 
-case 'p':
-    printf("\n %.2f * %.2f = %.2f\n", *p, *q, *p * *q);
-    break;
-}
-    
-    // }
-    // if( options == 'D' || options == 'd'){
-    //     printf("%.2f / %.2f = %.2f\n", *p, *q, *p / *q);
-    // }
-    
+    case 'p':
+        printf("\n %.2f * %.2f = %.2f\n", *p, *q, *p * *q);
+        break;
 
+    case 'D':
+        if (*q == 0)
+        {
+            printf("\nCan’t divide by 0");
+            break;
+        }
+        else
+        {
+            printf("%.2f / %.2f = %.2f\n", *p, *q, *p / *q);
+        }
 
+        break;
 
-    printf("\n \t Goodbye \n"); 
-    
+    case 'd':
+        if (*q == 0)
+        {
+            printf("\nCan’t divide by 0");
+            break;
+        }
+        else
+        {
+            printf("%.2f / %.2f = %.2f\n", *p, *q, *p / *q);
+        }
 
+        break;
+    }
 
-
-
-
+    printf("\n \t Goodbye \n");
 
     return 0;
 }
