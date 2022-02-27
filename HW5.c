99d243361528228b49149c28177cf8d679ab4719
Andrew Rodriguez <andrewrb22@gmail.com>
2022-02-27 16:39:14 -0500

    Validation for option character numbers only

--- HW5.c
+++ HW5.c
@@ -4,10 +4,13 @@
 int main()
 {
 
+//First number
     float *p;
+//Second Number    
     float *q;
+//Option character   
     char options;
-
+//Mmemory allocation for first(p) and second(q) number
     p = malloc(sizeof(*p));
     q = malloc(sizeof(*q));
 
@@ -17,14 +20,23 @@ int main()
     printf("Now, enter the following options:\na or A to show the sum\np or P to show the product\nd or D to show the division of the first by the second: ");
     scanf(" %c", &options);
 
+ if (options != 'A' && options != 'a' && options != 'p' && options != 'P' && options != 'D' && options != 'd')
+    {
+        printf("\n %c is not a valid option", options);
+    }
+    else
+    {
+//Switch case for options
     switch (options)
     {
+//Sum of options
     case 'A':
         printf("\n %.2f + %.2f is = %.2f\n", *p, *q, *p + *q);
         break;
     case 'a':
         printf("\n %.2f + %.2f is = %.2f\n", *p, *q, *p + *q);
         break;
+//Product of options        
     case 'P':
         printf("\n %.2f * %.2f = %.2f\n", *p, *q, *p * *q);
         break;
@@ -32,11 +44,11 @@ int main()
     case 'p':
         printf("\n %.2f * %.2f = %.2f\n", *p, *q, *p * *q);
         break;
-
+//Divide by options
     case 'D':
         if (*q == 0)
         {
-            printf("\nCan’t divide by 0");
+            printf("\nCan’t divide by 0\n");
             break;
         }
         else
@@ -49,7 +61,7 @@ int main()
     case 'd':
         if (*q == 0)
         {
-            printf("\nCan’t divide by 0");
+            printf("\nCan’t divide by 0\n");
             break;
         }
         else
@@ -59,7 +71,9 @@ int main()
 
         break;
     }
+}
 
+//Exit from the program
     printf("\n \t Goodbye \n");
 
     return 0;
